e813e4b93f40613086aa95f59cab79ee4182d460
GalacticGlow <96658580+GalacticGlow@users.noreply.github.com>
2024-05-15 18:47:04 +0300

    Update 100_movies_code.py
    Gen 2 version of the code

--- 100_movies_code.py
+++ 100_movies_code.py
@@ -4,35 +4,43 @@ import csv
 import os
 
 chosen_dir = input('Enter the path to the folder where you would like your files saved:')
-os.chdir(chosen_dir)
+
+while True:
+    try:
+        os.chdir(chosen_dir)
+        if not os.path.exists(chosen_dir+'\MovieFrameshots'):
+            os.mkdir(chosen_dir+'\MovieFrameshots')
+        break
+    except OSError:
+        print("The folder you chose doesn't exist or is incorrectly typed. Check if you typed the path correctly and try again")
+    chosen_dir = input('Enter the path to the folder where you would like your files saved:')
 
 response = requests.get('https://www.timeout.com/film/best-movies-of-all-time')
 soup = BeautifulSoup(response.text, 'html.parser')
 
-movie_titles_html = soup.find_all('h3', attrs={'data-testid':'tile-title_testID'})
+movie_titles_split_lists = [movie_title.text.split() for movie_title in soup.find_all('h3', attrs={'data-testid':'tile-title_testID'})]
 
 movie_titles = []
 movie_release_years = []
 
-for movie in movie_titles_html:
-    split_titles = movie.text.split()
-    titl = " ".join(split_titles[1:-1])
-    movie_titles.append(titl)
-    movie_release_years.append(split_titles[-1].replace('(', '').replace(')', ''))
+for movie_title_split_list in movie_titles_split_lists:
+    title = ' '.join(movie_title_split_list[1:-1])
+    movie_titles.append(title)
+    movie_release_years.append(movie_title_split_list[-1].replace('(', '').replace(')', ''))
 del movie_release_years[-1]
-del movie_titles[-1]
+del movie_titles[-1] 
 
 movie_links_html = soup.find_all('a', attrs={'data-testid':'tile-link_testID'})
 
 movie_links = []
-start_link = 'https://www.timeout.com'
+base_url = 'https://www.timeout.com'
 
 for i in range(len(movie_links_html)):
     if i%2 == 0:
         if movie_links_html[i]['href'] == '/movies':
             movie_links.append('-')
         else:
-            movie_links.append(start_link + movie_links_html[i]['href'])
+            movie_links.append(base_url + movie_links_html[i]['href'])
 del movie_links[-1]
 
 durations_list = []
@@ -56,8 +64,8 @@ image_url_list = []
 for article in article_list:
     article_links = article.find_all('a', attrs={'data-testid':'tile-link_testID'})
     found_images = False
-    for art_link in article_links:
-        if '/movies' in art_link['href']:
+    for article_link in article_links:
+        if '/movies' in article_link['href']:
             images = article.find_all('img')
             if images:
                 found_images = True
@@ -67,18 +75,16 @@ for article in article_list:
     if not found_images:
         image_url_list.append('-')
 
-fil = open('100 best movies of all time.csv', 'w', newline='')
-writer = csv.writer(fil)
-
+file = open('100 best movies of all time.csv', 'w', newline='')
+writer = csv.writer(file)
 writer.writerow(['Number', 'Title', 'Release year', 'Duration', 'Blurb'])
 
 number_column = [i+1 for i in range(len(image_url_list))]
 for num, title, release_year, duration, blurb in zip(number_column, movie_titles, movie_release_years, durations_list, movie_links):
     writer.writerow([num, title, release_year, duration, blurb])
-fil.close()
+file.close()
 
-if not os.path.exists(chosen_dir+'\MovieFrameshots'):
-    os.mkdir(chosen_dir+'\MovieFrameshots')
+already_saved_images = []
 os.chdir(chosen_dir+'\MovieFrameshots')
 for image_url in image_url_list:
     if image_url == '-':
@@ -86,7 +92,10 @@ for image_url in image_url_list:
     else:
         image = requests.get(image_url)
         image_name = str(image_url_list.index(image_url)+1) + '. ' + movie_titles[image_url_list.index(image_url)] + '.jpg'
+        if image_url in already_saved_images:
+            image_name = image_name + str(already_saved_images.count(image_url))
         if ':' in image_name:
             image_name = image_name.replace(':', '')
         with open(image_name, 'wb') as file:
             file.write(image.content)
+            already_saved_images.append(image_url)
